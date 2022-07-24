bcdbeb3ca1071a1f39bbd67beeb0e1c89b63e810
Op Pautu <pautuop@gmail.com>
2022-07-24 14:35:36 +0530

    add operate function

--- myScript.js
+++ myScript.js
@@ -1,6 +1,4 @@
-const add = function(a, b) {
-    return a+b
-}
+const add = (a, b) => a + b;
 
 const subtract = (a, b) => a - b;
 
@@ -9,3 +7,28 @@ const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
 
+const operate = function(operator, a, b) {
+    let result = ""
+    
+    switch(operator) {
+        case add:
+            result = add(a, b);
+            console.log(result);
+            break;
+        case subtract:
+            result = subtract(a, b);
+            console.log(result);
+            break;
+        case multiply:
+            result = multiply(a, b);
+            console.log(result);
+            break;
+        case divide:
+            result = divide(a, b);
+            console.log(result);
+            break;
+        
+    }
+}
+
+operate(multiply, 10, 10)
