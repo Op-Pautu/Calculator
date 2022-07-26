95efce8f04a543aad0811da05e142648a22443f8
Op Pautu <pautuop@gmail.com>
2022-07-26 22:24:01 +0530

    refactor code and add eventlistener

--- myScript.js
+++ myScript.js
@@ -1,3 +1,84 @@
+//global variables
+let dis1Num = '';
+let dis2Num = '';
+let result= null;
+let lastOperation = '';
+let haveDot = false;
+
+//selecting DOM
+const buttons = document.querySelectorAll('button');
+const numbers = document.querySelectorAll('#number');
+
+const operators = document.querySelectorAll('#operator')
+const lastScreen = document.querySelector('.screen-last')
+const currentScreen = document.querySelector('.screen-current')
+
+const equal = document.querySelector('.equal');
+
+// buttons.forEach(button => {
+//     button.addEventListener('click', () => {
+//         display.textContent += button.className;  
+//         let num1 = display.textContent;
+        
+//     })
+// })
+numbers.forEach(number => {
+    number.addEventListener('click', (e) => {
+        if (e.target.textContent === '.' && !haveDot) {
+            haveDot = true;
+            
+        }
+        else if (e.target.textContent === '.' && haveDot) {
+            return;
+        }
+        dis2Num += e.target.textContent;
+        currentScreen.textContent = dis2Num;
+
+        
+    })
+});
+
+    
+operators.forEach(operator => {
+    operator.addEventListener('click', (e) => {
+      if (!dis2Num) return;  
+      haveDot = false;
+      const chooseOperation = e.target.textContent;
+      if (dis1Num && dis2Num && lastOperation){
+        operate();
+      } else {
+        result = parseFloat(dis2Num)
+      }
+
+        
+    })
+}) 
+
+
+    
+
+// compute()
+// let chooseOperation = prompt('Choose an operator: ')
+// (operate(chooseOperation, num1, num2));
+
+
+
+
+//DOM
+const del = document.getElementById('delete');
+del.addEventListener('click', () => {
+   display.textContent = display.textContent.slice(0, display.textContent.length - 1)
+
+})
+
+const clear = document.getElementById('clear');
+clear.addEventListener('click', () => {
+    display.textContent = "";
+    
+
+})
+
+
 const add = (a, b) => a + b;
 
 const subtract = (a, b) => a - b;
@@ -7,48 +88,26 @@ const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
 
-const operate = function(operator, a, b) {
+function operate(operator, a, b) {
     
-    
+    let result = ""
     switch(operator) {
         case '+':
-            add(a, b);
+            result = add(a, b);
             break;
         case '-':
-            subtract(a, b);
+            result = subtract(a, b);
             break;
         case '*':
-            multiply(a, b);
+            result = multiply(a, b);
             break;
         case '÷':
-            divide(a, b);
+            if (b === 0) result = null;
+     
+            result = divide(a, b);
             break;
         default: 
             alert("invalid operator")
     }
+    return result;
 }
-
-const buttons = document.querySelectorAll('button');
-const space = document.querySelector('#space');
-
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        space.textContent += button.className;
-    })
-})
-
-const del = document.getElementById('delete');
-del.addEventListener('click', () => {
-   space.textContent = space.textContent.slice(0, space.textContent.length - 1)
-
-})
-
-const clear = document.getElementById('clear');
-clear.addEventListener('click', () => {
-    space.textContent = "";
-    
-
-})
-
-
-
